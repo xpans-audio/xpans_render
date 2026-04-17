e21ea83a8ad6cd1130f06fe8cd73acf9d234338b
ben <208722981+benseigler@users.noreply.github.com>
2026-04-17 17:53:23 -0400

    Add links to implementation examples

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ mode-agnostic renderers for the xpans Ecosystem
 
 [![Crates.io Version](https://img.shields.io/crates/v/xpans_render)](https://crates.io/crates/xpans_render)
 [![docs.rs](https://img.shields.io/docsrs/xpans_render)](https://docs.rs/xpans_render/0.1.0/xpans_render/)
+
+## Examples
+For examples on how to implement rendering modes, refer to
+[xpans Stereo](https://github.com/xpans-audio/xpans_stereo) and
+[xpans Headphones](https://github.com/xpans-audio/xpans_headphones).
+
+For an example on how to implement a renderer, refer to
+[xpans Violet](https://github.com/xpans-audio/xpans_violet).
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-//! An interface for defining spatial audio rendering modes and
-//! implementing mode-agnostic renderers for the xpans Ecosystem
+/*!
+An interface for defining spatial audio rendering modes and
+implementing mode-agnostic renderers for the xpans Ecosystem
+
+## Examples
+For examples on how to implement rendering modes, refer to
+[xpans Stereo](https://github.com/xpans-audio/xpans_stereo) and
+[xpans Headphones](https://github.com/xpans-audio/xpans_headphones).
+
+For an example on how to implement a renderer, refer to
+[xpans Violet](https://github.com/xpans-audio/xpans_violet).
+*/
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "std")]
